ea82ca76f15fb4b326c729f528f87dc495c30f57
p1716815x <p1716815x@my365.dmu.ac.uk>
2019-02-04 11:18:20 +0000

    Creation of clsCar + test class

---
+++ Test Framework/tstCar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstCar
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of our class clsCar
+            clsCar ACar = new clsCar();
+            //check to see that the class is not null
+            Assert.IsNotNull(ACar);
+        }
+    }
+}
