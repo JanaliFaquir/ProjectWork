a915617e3e74ab2571377757aa2fb0d65bbd14b4
p16225481 <p16225481@my365.dmu.ac.uk>
2019-03-04 11:59:58 +0000

    all man

--- Class Library/clsManufacturer.cs
+++ Class Library/clsManufacturer.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text; 
+using System.Text;
 
 namespace Class_Library
 {
@@ -13,20 +13,20 @@ namespace Class_Library
         private DateTime mYearMade;
         //private data member for the manufacturerid property
         private Int32 mManufacturerID;
-       //private data member for the batchproductionno property
+        //private data member for the batchproductionno property
         private Int32 mBatchProductionNo;
-       //private data member for the phoneno property
+        //private data member for the phoneno property
         private Int32 mPhoneNo;
-       //private data member for the carmodel property
+        //private data member for the carmodel property
         private String mCarModel;
         //private data member for the chairman property
         private String mChairMan;
         //private data member for the email property
-         private String mEmail;
+        private String mEmail;
 
-        public bool Available { get; set; }
-        public DateTime YearMade { get; set; }
-        public int ManufacturerID { get; set; }
+        //public bool Available { get; set; }
+        // public DateTime YearMade { get; set; }
+        //public int ManufacturerID { get; set; }
         public int BatchProductionNo { get; set; }
         public int PhoneNo { get; set; }
         public string CarModel { get; set; }
@@ -34,9 +34,62 @@ namespace Class_Library
         public string Email { get; set; }
 
         public bool Find(int ManufacturerID)
-        { //always return true 
+        {
+            //set the private data member to the test data value
+            mManufacturerID = 456;
+            mAvailable = true;
+            mYearMade = Convert.ToDateTime("04/03/2018");
+            //always return true
             return true;
+
         }
+
+        // public property for the manufacturer id 
+        public int ManufacturerID
+        {
+            get
+            {
+                //return the private data
+                return mManufacturerID;
+            }
+            set
+            {
+                //set the value of the private data member
+                mManufacturerID = value;
+            }
+        }
+
+        // public property for the available
+        public bool Available
+        {
+            get
+            {
+                //return the private data
+                return mAvailable;
+            }
+            set
+            {
+                //set the value of the private data member
+                mAvailable = value;
+            }
+        }
+
+        // public property for the yearmade
+        public DateTime YearMade
+        {
+            get
+            {
+                //return the private data
+                return mYearMade;
+            }
+            set
+            {
+                //set the value of the private data member
+                mYearMade = value;
+            }
+        }
+
+
     }
-   
+
 }
