ee17ade2395bc8fbc22cf1c396194e824170f03c
p16225481 <p16225481@my365.dmu.ac.uk>
2019-03-18 11:20:14 +0000

    everything

---
+++ Test Framework/clsManufacturerCollection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class_Library
+{
+    public class clsManufacturerCollection
+    {
+        private List<clsManufacturer> mManufacturerList;
+
+        public List<clsManufacturer> ManufacturerList
+        {
+            get
+            {
+                //return the private data 
+                return mManufacturerList;
+            }
+            set
+            {
+                //set the private data 
+                mManufacturerList = value;
+            }
+        }
+
+        //public property for count 
+        public int Count
+        {
+            get
+            {
+                //return the count of the list 
+                return mManufacturerList.Count;
+            }
+            set
+            {
+                //we shall worry about this later 
+            }
+        }
+
+        public clsManufacturer ThisManufacturer { get; set; }
+
+    }
+}
