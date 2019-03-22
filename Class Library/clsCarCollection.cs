7f7c3f4c7dccc174e8ffef20a13a938fed10f49b
p1716815x <p1716815x@my365.dmu.ac.uk>
2019-03-22 12:06:27 +0000

    finished changes

--- Class Library/clsCarCollection.cs
+++ Class Library/clsCarCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Class_Library
 {
@@ -6,6 +7,8 @@ namespace Class_Library
     {
         //private data member for the list
         List<clsCar> mCarList = new List<clsCar>();
+        //private data member thisCar
+        clsCar mThisCar = new clsCar();
 
         //public property for the car list
         public List<clsCar> CarList
@@ -35,7 +38,125 @@ namespace Class_Library
                 //we shall worry about this latter
             }
         }
-        public clsCar ThisCar { get; set; }
+
+        //public property for ThisCar
+        public clsCar ThisCar
+        {
+            get
+            {
+                //return the private data
+                return mThisCar;
+            }
+            set
+            {
+                //set the private data
+                mThisCar = value;
+            }
+        }
+
+        //constructor for the class
+        public clsCarCollection()
+        {
+            //object for the data Connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure 
+            DB.Execute("sproc_tblCar_SelectAll");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        public int Add()
+        {
+            //add a new record to the database based on the values of mThisCar
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@CarName", mThisCar.CarName);
+            DB.AddParameter("@Model", mThisCar.Model);
+            DB.AddParameter("@BodyType", mThisCar.BodyType);
+            DB.AddParameter("@ColourNo", mThisCar.ColourNo);
+            DB.AddParameter("@Price", mThisCar.Price);
+            DB.AddParameter("@Stock", mThisCar.Stock);
+            DB.AddParameter("@Fuel", mThisCar.Fuel);
+            DB.AddParameter("@YearMade", mThisCar.YearMade);
+            //execute the query returning the primary key value
+            return DB.Execute("sproc_tblCar_Insert");
+        }
+
+        public void DeleteCar()
+        {
+            //deletes the record pointed to by thisCar
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("CarID", mThisCar.CarID);
+            //execute the store procedure
+            DB.Execute("sproc_tblCar_Delete");
+        }
+
+        public void Update()
+        {
+            //Update a existing record to the database based on the values of thisCar
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@CarID", mThisCar.CarID);
+            DB.AddParameter("@CarName", mThisCar.CarName);
+            DB.AddParameter("@Model", mThisCar.Model);
+            DB.AddParameter("@BodyType", mThisCar.BodyType);
+            DB.AddParameter("@ColourNo", mThisCar.ColourNo);
+            DB.AddParameter("@Price", mThisCar.Price);
+            DB.AddParameter("@Stock", mThisCar.Stock);
+            DB.AddParameter("@Fuel", mThisCar.Fuel);
+            DB.AddParameter("@YearMade", mThisCar.YearMade);
+            //execute the stored procedure
+            DB.Execute("sproc_tblCar_Update");
+        }
+
+        public void ReportByModel(string Model)
+        {
+            //filters the records based on a full or partial model
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //send the model parameter to the database
+            DB.AddParameter("@Model", Model);
+            //execute the stored procedure
+            DB.Execute("sproc_tblCar_FilterByModel");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //populates the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount;
+            //get the count of records
+            RecordCount = DB.Count;
+            //clear the private array list
+            mCarList = new List<clsCar>();
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank car
+                clsCar ACar = new clsCar();
+                //read in the field from the current record
+                ACar.Fuel = Convert.ToBoolean(DB.DataTable.Rows[Index]["Fuel"]);
+                ACar.CarID = Convert.ToInt32(DB.DataTable.Rows[Index]["CarID"]);
+                ACar.ColourNo = Convert.ToInt32(DB.DataTable.Rows[Index]["ColourNo"]);
+                ACar.CarName = Convert.ToString(DB.DataTable.Rows[Index]["CarName"]);
+                ACar.Model = Convert.ToString(DB.DataTable.Rows[Index]["Model"]);
+                ACar.BodyType = Convert.ToString(DB.DataTable.Rows[Index]["BodyType"]);
+                ACar.Price = Convert.ToDecimal(DB.DataTable.Rows[Index]["Price"]);
+                ACar.Stock = Convert.ToInt32(DB.DataTable.Rows[Index]["Stock"]);
+                ACar.YearMade = Convert.ToDateTime(DB.DataTable.Rows[Index]["YearMade"]);
+                mCarList.Add(ACar);
+                //point at the next record
+                Index++;
+            }
+        }
 
     }
 }
---
+++ Class Library/clsColour.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Library
+{
+    public class clsColour
+    {
+        //private data member for the colour no property
+        private int mColourNo;
+        //private data member for the Colour Name property
+        private string mColourName;
+
+        //public roperty for the ColourNo
+        public int ColourNo
+        {
+            get
+            {
+                //return the private data
+                return mColourNo;
+            }
+            set
+            {
+                //set the private data
+                mColourNo = value;
+            }
+        }
+        //public roperty for the CarName
+        public string ColourName
+        {
+            get
+            {
+                //return the private data
+                return mColourName;
+            }
+            set
+            {
+                //set the private data
+                mColourName = value;
+            }
+        }
+
+        public string Valid(string someColourName)
+        {
+            //sttring variable to store the error message
+            string Error = "";
+            //if the name of the colour is more than 16
+            if (someColourName.Length > 16)
+            {
+                //return an error message
+                Error = "the colour name cannot be more than 16 characters";
+            }
+            if (someColourName.Length == 0)
+            {
+                //return an error message
+                Error = "the colour name may not be blank!";
+            }
+            return Error;
+        }
+
+        public bool Find(int colourNo)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the colour no to search for
+            DB.AddParameter("@ColourNo", ColourNo);
+            //execute the store procedure
+            DB.Execute("sproc_tblColour_FilterByColourNo");
+            //if one record is found (there should be either one or zero)
+            if(DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mColourNo = Convert.ToInt32(DB.DataTable.Rows[0]["ColourNo"]);
+                mColourName = Convert.ToString(DB.DataTable.Rows[0]["ColourName"]);
+
+                //return that everything worked OK
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return false indicating a problem
+                return false;
+            }
+        }
+    }
+}
